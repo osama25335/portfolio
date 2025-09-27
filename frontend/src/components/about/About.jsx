f9c09aa07ed59f26324de86108bddc3f3428faa3
osama25335 <khelifabaghdoucheoussama0@gmail.com>
2025-09-27 14:53:47 +0100

    update cv

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import './about.css';
 import AboutImg from '../../assets/about.jpg';
-import CV from '../../assets/cv.pdf';
 import Info from './Info';
+import cv from '../../assets/cv.pdf'
 import { FileText } from 'lucide-react';
 function About() {
   return (
@@ -15,7 +15,7 @@ function About() {
               <p className='about__description'>
                  I specialize in building smart, efficient online solutions—like custom forms, fillable PDFs, and automation tools—while applying my expertise in cybersecurity to ensure data privacy and safe digital workflows. With a proactive mindset and a passion for secure, modern tech, I help businesses thrive in a digital-first world.
               </p>
-              <a download href={CV} className="button button_flex">Download CV<FileText className='button__icon'/></a>
+              <a download href={cv} className="button button_flex">Download CV<FileText className='button__icon'/></a>
             </div>
         </div>
     </section>
