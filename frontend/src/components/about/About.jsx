058277a5f0c33ce91b6356bf5a28057756d40094
osama25335 <khelifabaghdoucheoussama0@gmail.com>
2025-09-27 14:59:08 +0100

    update cv

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import './about.css';
 import AboutImg from '../../assets/about.jpg';
 import Info from './Info';
-import cv from '../../assets/cv.pdf'
+import cv from './cv.pdf'
 import { FileText } from 'lucide-react';
 function About() {
   return (
